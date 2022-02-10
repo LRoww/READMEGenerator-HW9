84617469cfada546a07c2182072e5773539e5729
Laura Rowe <laurarowe96@gmail.com>
2022-02-09 21:07:36 -0330

    added write function and text content questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,47 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = ["Title:", "Description:", "Installation instructions:", "Usage instructions and examples:", "Who contributed:", "Tests instructions:"];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Title of your application:',
+  },
+  {
+    type: 'input',
+    name: 'discription',
+    message: 'Description:',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'Installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage instructions and examples:',
+  },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Who contributed:',
+  },
+    {
+    type: 'input',
+    name: 'test',
+    message: 'Tests instructions:',
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.appendFile(fileName, data, (err) =>
+  err ? console.error(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
 function init() {}

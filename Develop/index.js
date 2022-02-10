e0c959a60184f986a1b4bf7d8bc400273cf560e2
Laura Rowe <laurarowe96@gmail.com>
2022-02-10 17:11:44 -0330

    edited questions and added call to ask questions and generate blank markdown to init

--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,45 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 // const questions = ["Title:", "Description:", "Installation instructions:", "Usage instructions and examples:", "Who contributed:", "Tests instructions:"];
-const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Title of your application:',
-  },
-  {
-    type: 'input',
-    name: 'discription',
-    message: 'Description:',
-  },
-  {
-    type: 'input',
-    name: 'install',
-    message: 'Installation instructions:',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Usage instructions and examples:',
-  },
-  {
-    type: 'input',
-    name: 'contributors',
-    message: 'Who contributed:',
-  },
+const questions = () => {
+  return inquirer.prompt([
     {
-    type: 'input',
-    name: 'test',
-    message: 'Tests instructions:',
-  },
-];
+      type: 'input',
+      name: 'title',
+      message: 'Title of your application:',
+    },
+    {
+      type: 'input',
+      name: 'discription',
+      message: 'Description:',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Usage instructions and examples:',
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Who contributed:',
+    },
+      {
+      type: 'input',
+      name: 'test',
+      message: 'Tests instructions:',
+    },
+  ]);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -44,7 +48,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+  .then((answers) => writeToFile("README.md", generateMarkdown(answers)))
+  .then((answers) => console.log(answers))
+}
 
 // Function call to initialize app
 init();
